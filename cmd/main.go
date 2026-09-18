c164fe61f8d6324368c9554764cd12f02578a8eb
arthur <arthur.broudoux@gmail.com>
2026-09-18 02:58:15 -0700

    feature: init backend

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/charmbracelet/log"
+
+	"github.com/traily-org/server/internal/adapters/http"
+	"github.com/traily-org/server/internal/config"
+	"github.com/traily-org/server/internal/domain/greeting"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	cfg := config.Load()
+
+	greetingService := greeting.NewService()
+	greetingHandler := http.NewGreetingHandler(greetingService)
+	server := http.NewServer(greetingHandler)
+
+	if err := server.Run(cfg.Port); err != nil {
+		log.Fatalf("server stopped: %v", err)
+	}
 }
